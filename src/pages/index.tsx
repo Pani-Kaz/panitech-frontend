bbcef99ebe135d9e4e31ac2ab57586b819ff5638
Kaua Felipe <kauaf@xgrow.com>
2024-09-22 02:43:37 -0300

    feat: typebot action in landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,28 @@ import { Main } from "@/components/common/main";
 import { buttonVariants } from "@/components/ui/button";
 import { Icon } from "@iconify/react/dist/iconify.js";
 import Link from "next/link";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
+import { Standard } from "@typebot.io/nextjs";
+import dynamic from "next/dynamic";
+const Carousel = dynamic(
+  () => import("@/components/ui/carousel").then((mod) => mod.Carousel),
+  { ssr: false }
+);
+const CarouselContent = dynamic(
+  () => import("@/components/ui/carousel").then((mod) => mod.CarouselContent),
+  { ssr: false }
+);
+const CarouselItem = dynamic(
+  () => import("@/components/ui/carousel").then((mod) => mod.CarouselItem),
+  { ssr: false }
+);
+const CarouselNext = dynamic(
+  () => import("@/components/ui/carousel").then((mod) => mod.CarouselNext),
+  { ssr: false }
+);
+const CarouselPrevious = dynamic(
+  () => import("@/components/ui/carousel").then((mod) => mod.CarouselPrevious),
+  { ssr: false }
+);
 import { ImagePaginator } from "@/components/common/ImagePaginator";
 
 export default function Page() {
@@ -74,7 +89,10 @@ export default function Page() {
           className="w-full max-w-[95%] max-lg:w-[80%]"
         >
           <CarouselContent className="max-lg:gap-2 px-6">
-            <CarouselItem className="md:basis-1/2 lg:basis-1/3 mx-1 max-lg:mx-0 min-h-14 rounded-xl border-zinc-200 border border-solid p-0 duration-500 transition-all hover:border-blue-300">
+            <CarouselItem
+              key={1}
+              className="md:basis-1/2 lg:basis-1/3 mx-1 max-lg:mx-0 min-h-14 rounded-xl border-zinc-200 border border-solid p-0 duration-500 transition-all hover:border-blue-300"
+            >
               <Link
                 href={"/bom-chat"}
                 className="flex p-5 w-full h-full flex-col justify-between gap-3 group"
@@ -101,7 +119,10 @@ export default function Page() {
                 </Link>
               </Link>
             </CarouselItem>
-            <CarouselItem className="md:basis-1/2 lg:basis-1/3 mx-1 max-lg:mx-0 min-h-14 rounded-xl border-zinc-200 border border-solid p-0 duration-500 transition-all hover:border-blue-300">
+            <CarouselItem
+              key={2}
+              className="md:basis-1/2 lg:basis-1/3 mx-1 max-lg:mx-0 min-h-14 rounded-xl border-zinc-200 border border-solid p-0 duration-500 transition-all hover:border-blue-300"
+            >
               <Link
                 href={"/artificial-intelligence-assistant"}
                 className="flex p-5 w-full h-full flex-col justify-between gap-3 group"
@@ -128,7 +149,10 @@ export default function Page() {
                 </Link>
               </Link>
             </CarouselItem>
-            <CarouselItem className="md:basis-1/2 lg:basis-1/3 mx-1 max-lg:mx-0 min-h-14 rounded-xl border-zinc-200 border border-solid p-0 duration-500 transition-all hover:border-blue-300">
+            <CarouselItem
+              key={3}
+              className="md:basis-1/2 lg:basis-1/3 mx-1 max-lg:mx-0 min-h-14 rounded-xl border-zinc-200 border border-solid p-0 duration-500 transition-all hover:border-blue-300"
+            >
               <Link
                 href={"/sites"}
                 className="flex p-5 w-full h-full flex-col justify-between gap-3 group"
@@ -155,7 +179,10 @@ export default function Page() {
                 </Link>
               </Link>
             </CarouselItem>
-            <CarouselItem className="md:basis-1/2 lg:basis-1/3 mx-1 max-lg:mx-0 min-h-14 rounded-xl border-zinc-200 border border-solid p-0 duration-500 transition-all hover:border-blue-300">
+            <CarouselItem
+              key={4}
+              className="md:basis-1/2 lg:basis-1/3 mx-1 max-lg:mx-0 min-h-14 rounded-xl border-zinc-200 border border-solid p-0 duration-500 transition-all hover:border-blue-300"
+            >
               <Link
                 href={"/crm"}
                 className="flex p-5 w-full h-full flex-col justify-between gap-3 group"
@@ -203,7 +230,54 @@ export default function Page() {
           Veja nossas principais ferramentas atualmente ativas no mercado
         </p>
         <div className="w-[40%] max-lg:w-[95%]">
-          <ImagePaginator images={["/assets/images/dashboard.svg", "/assets/images/crm.jpg"]} />
+          <ImagePaginator
+            images={["/assets/images/dashboard.svg", "/assets/images/crm.jpg"]}
+          />
+        </div>
+      </section>
+      <section className="w-full py-10 grid grid-cols-2 max-lg:grid-cols-1 gap-2 text-black">
+        <div className="flex flex-col gap-4 px-[10%] max-lg:px-2 py-4 order-2 max-lg:order-1">
+          <div className="flex items-center gap-2 text-blue-800">
+            <Icon
+              className="bg-blue-300/50 outline outline-4 outline-blue-300/50 rounded-[6px]"
+              icon="fluent:chat-24-regular"
+              width="24"
+              height="24"
+            />
+            <p className="text-xl font-extrabold">Bate papo em tempo real</p>
+          </div>
+          <h2 className="font-extrabold text-4xl">
+            Utilize de funções para melhorar o atendimento ao cliente
+          </h2>
+          <p className="font-bold opacity-75 text-sm">
+            Faça integração com inúmeras plataformas, utilize automações
+            prontas, chatbots e muito mais.
+          </p>
+          <div>
+            <Link
+              href={"/register"}
+              className={`${buttonVariants()} py-5 px-6 text-base font-extrabold !bg-blue-600 !cursor-pointer duration-500 transition-transform hover:scale-105 hover:!bg-blue-700 hover:text-white`}
+            >
+              Realizar teste grátis
+            </Link>
+          </div>
+          <p className="pt-8 font-bold uppercase text-xs opacity-75">Integre com:</p>
+          <div className="w-full flex items-center justify-between gap-1">
+            <span className="w-[15%] h-[1px] bg-black opacity-10"/>
+            <Icon  className="text-green-500" icon="ic:baseline-whatsapp" width="24" height="24" />
+            <Icon className="text-pink-500" icon="mdi:instagram" width="24" height="24" />
+            <Icon className="text-blue-500" icon="ic:baseline-facebook" width="24" height="24" />
+            <Icon className="text-red-500" icon="ic:baseline-email" width="24" height="24" />
+            <Icon icon="logos:openai" width="46" height="24" />
+            <span className="w-[15%] h-[1px] bg-black opacity-10"/>
+          </div>
+        </div>
+        <div className="w-full bg-zinc-100 rounded-r-2xl min-h-[60vh] outline-[15px] outline outline-blue-400/10 border-r border-t border-b border-solid border-blue-200 overflow-hidden bg-[url('/assets/images/background.png')] max-lg:border-none max-lg:outline-none max-lg:shadow-none max-lg:rounded-none order-1 max-lg:order-2">
+          <Standard
+            typebot="website-panitech"
+            apiHost="https://typebotapi.panitech.com.br"
+            style={{ width: "100%", height: "600px" }}
+          />
         </div>
       </section>
     </Main>
